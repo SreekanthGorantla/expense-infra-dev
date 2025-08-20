37275027a17bfdfb705e081d333491b1c400ab48
Sreekanth Gorantla <gorantlasreekanth@gmail.com>
2025-08-20 12:43:37 -0500

    expense-infra-dev recommit

--- 80-web-alb/main.tf
+++ 80-web-alb/main.tf
@@ -38,7 +38,7 @@ resource "aws_lb_listener" "front_end" {
 
 resource "aws_route53_record" "web_alb" {
   zone_id = var.zone_id
-  name    = "*.${var.domain_name}"
+  name    = "expense-${var.environment}.${var.domain_name}"
   type    = "A"
 
   alias {
